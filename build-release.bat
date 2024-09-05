9b323fccc732f637bfacc0a1bab737cb33fadb54
Octavian Cota <octavian.cota@cdev.ro>
2024-09-05 11:20:58 +0300

    Normalize file paths in the program to use forward slashes instead of backslashes

---
+++ build-release.bat
@@ -0,0 +1,5 @@
+@echo off
+pushd "%~dp0"
+dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+copy bin\Release\net8.0\win-x64\publish\PyToPdf.exe ..\..\dwf981@bb\Main\Developer\buildsystem\bin\
+popd
